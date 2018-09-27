44de6ab1ca5866e16cd52b11aaaf988d55afad27
EdmundMartin <edmartin101@googlemail.com>
2018-09-27 20:11:44 +0100

    Added mongo mixins

---
+++ scrapio/mixins/mongo.py
@@ -0,0 +1,29 @@
+from collections import Iterable
+import logging
+
+try:
+    import motor.motor_asyncio as motor_async
+except ImportError:
+    ImportError("Motor is required to use MongoMixins")
+
+
+class MongoMixin:
+
+    def create_mongo_client(self, connection_string: str, database: str, collection: str) -> None:
+        client = motor_async.AsyncIOMotorClient(connection_string)
+        database = client[database]
+        self.__setattr__('mongo_connection', database[collection])
+
+    async def save_results(self, result):
+        connection = self.__getattribute__('mongo_connection')
+        if isinstance(result, Iterable):
+            try:
+                await connection.insert_many(result)
+            except Exception as e:
+                logger = logging.getLogger('Scraper')
+                logger.warning('Exception inserting documents into collection: {}'.format(e))
+        try:
+            await connection.insert(result)
+        except Exception as e:
+            logger = logging.getLogger('Scraper')
+            logger.warning('Exception inserting document into collection: {}'.format(e))
