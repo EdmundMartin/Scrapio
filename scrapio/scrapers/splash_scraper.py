1f55f11f8650502466106e9ceaf9ee6662597b8b
EdmundMartin <edmartin101@googlemail.com>
2018-10-11 20:53:49 +0100

    Removed two seperate queues

--- scrapio/scrapers/splash_scraper.py
+++ scrapio/scrapers/splash_scraper.py
@@ -7,7 +7,7 @@ from aiohttp import ClientSession, ClientTimeout, ClientResponse
 
 from scrapio.structures.filtering import URLFilter
 from scrapio.parsing.links import link_extractor
-from scrapio.structures.queues import URLQueue, TimeoutQueue
+from scrapio.structures.queues import JobQueue
 from scrapio.requests.get import get_with_splash
 from scrapio.structures.proxies import AbstractProxyManager
 from scrapio.utils.helpers import create_client_session
@@ -58,14 +58,23 @@ class SplashScraper:
             self._url_filter = URLFilter(start_url, kwargs.get('additional_rules', []),
                                          kwargs.get('follow_robots', True))
 
-        self._request_queue = URLQueue(max_crawl_size)
-        self._parse_queue = TimeoutQueue()
+        self._task_queue = JobQueue(max_crawl_size=max_crawl_size)
 
         self._executor = kwargs.get('executor', None)
         logging.basicConfig(level=logging.DEBUG, format='%(message)s')
         self._logger = kwargs.get('logger', logging.getLogger("Scraper"))
 
         self.__remaining_coroutines = 0
+
+    def _get_best_event_loop(self):
+        try:
+            import uvloop
+        except ImportError:
+            self._logger.info('No UVLoop reverting to base event loop implementation')
+            return asyncio.get_event_loop()
+        else:
+            asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+            return asyncio.get_event_loop()
 
     @classmethod
     def create_scraper(cls, *args, timeout=30, user_agent=None, **kwargs):
@@ -79,9 +88,9 @@ class SplashScraper:
             self._client_timeout = ClientTimeout(total=float(self._timeout))
         if isinstance(self._start_url, list):
             for i in self._start_url:
-                self._request_queue.put_nowait(i)
+                self._task_queue.put_nowait(('Request', i))
         elif isinstance(self._start_url, str):
-            self._request_queue.put_nowait(self._start_url)
+            self._task_queue.put_nowait(('Request', self._start_url))
         return self
 
     def parse_result(self, response: ClientResponse) -> Any:
@@ -90,21 +99,17 @@ class SplashScraper:
     async def save_results(self, result):
         raise NotImplementedError
 
-    async def __consume_request_queue(self, consumer: int):
+    async def __consume_queue(self, consumer: int):
         self.__remaining_coroutines += 1
         while True:
             try:
-                url = await self._request_queue.get_max_wait(30)
-                self._logger.info('Thread: {}, Requesting URL: {}'.format(consumer, url))
-                splash_location = self.splash_configuration.splash_url.format(url=url)
-                if self._proxy_manager:
-                    proxy = await self._proxy_manager.get_proxy()
-                    if proxy:
-                        splash_location += '&proxy={}'.format(proxy)
-                resp = await get_with_splash(self._client, self._client_timeout, splash_location)
-                resp._url = url # Splash unfortunately loses the URL redirect information from the request
-                self._parse_queue.put_nowait(resp)
-            except asyncio.TimeoutError:
+                task, item = await self._task_queue.get_next_job()
+                if task == 'Request':
+                    await self._make_requests(consumer, item)
+                else:
+                    await self._parse_response(consumer, item)
+                self._task_queue.completed_task()
+            except asyncio.QueueEmpty:
                 self._logger.info('Thread: {}, No more URLs, Consumer shutting down'.format(consumer))
                 self.__remaining_coroutines -= 1
                 if self.__remaining_coroutines <= 0:
@@ -112,31 +117,38 @@ class SplashScraper:
                 return
             except Exception as e:
                 self._logger.warning("Thread: {}, Encountered exception: {}".format(consumer, e))
+                self._task_queue.completed_task()
 
-    async def __consume_parse_queue(self, consumer: int) -> None:
-        while True:
-            try:
-                loop = asyncio.get_event_loop()
-                resp = await self._parse_queue.get_max_wait(30)
-                links = await loop.run_in_executor(self._executor, link_extractor, resp, self._url_filter)
-                parsed_data = await loop.run_in_executor(self._executor, self.parse_result, resp)
-                await self.save_results(parsed_data)
-                for link in links:
-                    await self._request_queue.put_unique_url(link)
-            except asyncio.TimeoutError:
-                self._logger.info('Parser: {}, Message: No more Responses, consumer shutting down'.format(consumer))
-                return
-            except Exception as e:
-                self._logger.warning('Parser: {}, Encountered exception: {}'.format(consumer, e))
-
-    def run_scraper(self, request_workers: int, parse_workers: int) -> None:
-        print('running')
-        request_group = asyncio.gather(*[self.__consume_request_queue(i) for i in range(request_workers)])
-        parser_group = asyncio.gather(*[self.__consume_parse_queue(i) for i in range(parse_workers)])
-
-        loop = asyncio.get_event_loop()
+    async def _make_requests(self, consumer: int, url: str):
         try:
-            all_groups = asyncio.gather(request_group, parser_group)
-            loop.run_until_complete(all_groups)
+            self._logger.info('Thread: {}, Requesting URL: {}'.format(consumer, url))
+            splash_location = self.splash_configuration.splash_url.format(url=url)
+            if self._proxy_manager:
+                proxy = await self._proxy_manager.get_proxy()
+                if proxy:
+                    splash_location += '&proxy={}'.format(proxy)
+            resp = await get_with_splash(self._client, self._client_timeout, splash_location)
+            resp._url = url # Splash unfortunately loses the URL redirect information from the request
+            self._task_queue.put_nowait(('Parse', resp))
+        except Exception as e:
+            self._logger.warning("Thread: {}, Encountered exception: {}".format(consumer, e))
+
+    async def _parse_response(self, consumer: int, response: ClientResponse) -> None:
+        try:
+            loop = asyncio.get_event_loop()
+            links = await loop.run_in_executor(self._executor, link_extractor, response, self._url_filter)
+            parsed_data = await loop.run_in_executor(self._executor, self.parse_result, response)
+            await self.save_results(parsed_data)
+            for link in links:
+                await self._task_queue.put_unique_url(link)
+        except Exception as e:
+            self._logger.warning('Parser: {}, Encountered exception: {}'.format(consumer, e))
+
+    def run_scraper(self, workers: int) -> None:
+        work_group = asyncio.gather(*[self.__consume_queue(i) for i in range(workers)])
+
+        loop = self._get_best_event_loop()
+        try:
+            loop.run_until_complete(work_group)
         finally:
             loop.close()
